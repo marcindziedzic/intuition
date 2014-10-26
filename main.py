439cdd2e9589885f8266ed6816494606f31041a3
Marcin Dziedzic <marcin.dziedzic@pragmaticcoders.com>
2014-10-26 19:26:29 +0100

    deploying on heroku

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import os.path
+import os
 
 import motor
-
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url
+
 from intuition.handlers.board import BoardDefaultsHandler, BoardHandler
 
 
-db = motor.MotorClient().intuition
+if os.environ.get('MONGOHQ_URL'):
+    print('Connecting to mongo using: ', os.environ['MONGOHQ_URL'])
+    db = motor.MotorClient(os.environ['MONGOHQ_URL']).intuition
+else:
+    print('Connecting to local mongo instance')
+    db = motor.MotorClient().intuition
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
