ab1992f4d135db440b53c873ff60df34d4ef3a5a
Marcin Dziedzic <marcin.dziedzic@pragmaticcoders.com>
2014-11-18 22:01:48 +0100

    nodejs and npm setup script

--- bin/setup_dev_env.sh
+++ bin/setup_dev_env.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 ENV_PATH=~/.virtualenvs/intuition
 
-# setup nodejs
 # setup mongodb
 
-function setup_python {
+function setup_nodejs_and_npm() {
+    sudo apt-add-repository ppa:chris-lea/node.js &&
+    sudo apt-get update &&
+    sudo apt-get install nodejs &&
+    sudo npm install -g npm
+}
+
+function setup_python() {
     sudo apt-get install python3.4-dev
 }
 
-function setup_venv {
+function setup_venv() {
     pyvenv-3.4 --without-pip ${ENV_PATH}
     source ${ENV_PATH}/bin/activate
     cd ${ENV_PATH}
@@ -26,3 +32,4 @@ function setup_venv {
 
 setup_python
 setup_venv
+setup_nodejs_and_npm
